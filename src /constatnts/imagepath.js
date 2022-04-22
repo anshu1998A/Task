951284b20a760cad2c7aab3ce8e053b052958140
admin <admin@admins-iMac.local>
2022-04-22 10:38:37 +0530

    changes

--- src /constatnts/imagepath.js
+++ src /constatnts/imagepath.js
@@ -1,6 +1,8 @@
 export default {
-    plus: require('../assets/images/3524388.png'),
+    // plus: require('../assets/images/3524388.png'),
+    plus:{uri:"https://image.shutterstock.com/image-vector/plus-icon-flat-design-style-260nw-589199351.jpg"},
     delete: require('../assets/images/484662.png'),
-   edit:require('../assets/images/external-edit-interface-kiranshastry-lineal-color-kiranshastry.png'),
+//    edit:require('../assets/images/external-edit-interface-kiranshastry-lineal-color-kiranshastry.png'),
+   edit:{uri:"https://www.freeiconspng.com/thumbs/edit-icon-png/edit-icon-png-24.png"},
     delete1 :{uri:"https://image.shutterstock.com/image-vector/delete-icongarbage-trash-can-rubbish-260nw-1211550652.jpg"}
 }
